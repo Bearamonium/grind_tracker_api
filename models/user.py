e64625c273b11d682d9cdacc349e9eac4232fbf6
Philip Mills <philip.mills1998@outlook.com>
2024-03-21 00:12:42 +1100

    create user login and authorisation routes and information, alongside with error handling for incorrect data entry

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 from init import db, ma
 from marshmallow import fields
+from marshmallow.validate import Length, Regexp, And
 
 class User(db.Model):
     __tablename__ = "user"
@@ -8,3 +9,12 @@ class User(db.Model):
     username = db.Column(db.String(25), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
+
+
+class UserSchema(ma.Schema):
+
+    class Meta:
+        fields = ('id', 'name', 'email', 'password')
+
+user_schema = UserSchema(exclude=['password'])
+users_schema = UserSchema(many=True, exclude=['password'])
